3e5780518fa149efd3072b0dd1e428a40919de99
AngryPanda <287036406@qq.com>
2019-10-22 11:34:22 +0800

    20191022

--- src/screen/TransportSock/index.js
+++ src/screen/TransportSock/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Icon, Modal } from 'antd-mobile';
+import { Icon, Modal, Badge, Drawer, List } from 'antd-mobile';
 import { connect } from 'react-redux';
 import { BaiduMap, Overlay } from 'react-baidu-maps';
 import Fields from '@/component/Fields';
@@ -7,6 +7,8 @@ import Screen from '@/component/Screen';
 import CenterLoading from '@/component/CenterLoading';
 import { mapLoading, mapEffects } from '@/utils';
 import styles from '@/style/map.module.less';
+import { BRAND_COLOR } from '@/constants/color';
+import Empty from '@/component/Empty';
 
 const mapStateToProps = ({ sock }) => {
   return {
@@ -22,23 +24,22 @@ const mapDispatchToProps = ({ sock }) => ({
 });
 
 @connect(mapStateToProps, mapDispatchToProps)
-class WharfSock extends Component {
+class TransportSock extends Component {
+  zoom = 6
   state = {
     center: {lng:121.721133,lat:30.605635},
     visible: false,
-    detail: {}
+    detail: {},
+    list: false,
+    zoom: this.zoom,
+    zIndex: 0,
+    open: false
   }
   overlays = []
-  removeAndAddEvent(type) {
-    this.locationItems = document.querySelectorAll('.loc-item')
-    this.locationItems.forEach(item => {
-      item[`${type}EventListener`]('click', this.handleOverlaySelected, true)
-    });
-  }
   componentDidMount() { 
     this.props.fetchIntransitSock({}, data => {
       this.setState({
-        center: {lng:data[0].longitude,lat:data[0].latitude}
+        //center: {lng:data[0].longitude,lat:data[0].latitude}
       });
     });
   }
@@ -58,10 +59,11 @@ class WharfSock extends Component {
     self.map = map;
     const div = document.createElement('div');
     div.className = styles.ship;
+    div.dataset.index = self.index;
     const text = document.createElement('div');
-    text.className = 'loc-item'
+    text.className = 'loc-item';
     text.dataset.index = self.index;
-    text.innerHTML = `${self.text}<br/>${self.extra}`
+    text.innerHTML = `${self.text}<br/>${self.extra}`;
     div.appendChild(text);
     map.getPanes().labelPane.appendChild(div);
     div.addEventListener('touchstart', this.handleOverlaySelected, false)
@@ -75,7 +77,7 @@ class WharfSock extends Component {
     const pixel = map.pointToOverlayPixel(self.point);
     self.div.style.left = `${pixel.x-self.div.offsetWidth/2}px`;
     self.div.style.top = `${pixel.y-self.div.offsetHeight/2}px`;
-    self.text.style.marginLeft = `-${self.text.offsetWidth/2}px`
+    self.text.style.marginLeft = `-${self.text.offsetWidth/2}px`;
   }
   handleShowModal = flag => {
     this.setState({
@@ -88,10 +90,59 @@ class WharfSock extends Component {
     const transportIndex = target.dataset.index;
     let zIndex = target.style.zIndex || 1;
     target.style.zIndex = ++zIndex;
+    console.log(1)
     this.handleShowModal(true);
     this.setState({
       detail: list[transportIndex]
     });
+  }
+  handleMapZoom = type => {
+    const zoom = type ? (this.zoom >= 15 ? 15 : ++this.zoom) : (this.zoom <=5 ? 5 : --this.zoom);
+    this.setState({
+      zoom
+    })
+  }
+  handleListOpenChange = () => {
+    this.setState({
+      list: !this.state.list,
+      zIndex: !this.state.list ? 10 : 0
+    })
+  }
+  handleTransportSeleted = item => {
+    const { longitude, latitude } = item;
+    console.log([longitude, latitude])
+    this.setState({
+      center: {lng:longitude,lat:latitude},
+      list: false,
+      visible: true,
+      detail: item,
+      zIndex: 0
+    })
+  }
+  renderTransportList = () => {
+    const { sock: {list} } = this.props;
+    return (
+      <List style={{width: 300}}>
+        {
+          list.length ? 
+          list.map(item => (
+            <List.Item 
+              key={`lng${item.longitude}_lat${item.latitude}`} 
+              extra={
+                <span className='text-primary'>
+                  <b>{item.weight || '0'}</b>吨
+                </span>
+              }
+              onClick={() => this.handleTransportSeleted(item)}
+            >
+              {item.cargoShortName}
+              <List.Item.Brief>{item.vesselExportEname || item.vesselImportEname || '未知'}/{item.exportVoyage || item.importVoyage || '未知'}</List.Item.Brief>
+            </List.Item>
+          ))
+          : <Empty/>
+        }
+      </List>
+    )
   }
   render(){
     // eslint-disable-next-line
@@ -110,10 +161,10 @@ class WharfSock extends Component {
         title: '重量',
         dataIndex: 'weight'
       },
-      {
+      /* {
         title: '扣除损耗商检量',
         dataIndex: 'feeWeight'
-      },
+      }, */
       {
         title: '上一港',
         dataIndex: 'portName1'
@@ -150,34 +201,52 @@ class WharfSock extends Component {
             <div className={styles.backButton} onClick={e => history.goBack()}>
               <Icon type='left' size='f'/>
             </div>
+            <div className={styles.mapControl}>
+              <div className={styles.zoom}>
+                <span onClick={() => this.handleMapZoom(true)}>+</span>
+                <span onClick={() => this.handleMapZoom(false)}>-</span>
+              </div>
+              {
+                list && list.length ? 
+                <div className={styles.toggleList}>
+                  <span className='mt8' onClick={this.handleListOpenChange}>
+                    <Badge text={list.length}>
+                      <Icon type='liebiao' color={this.state.list ? BRAND_COLOR : ''}/>
+                    </Badge>
+                  </span>
+                </div> : null
+              }
+            </div>
           </div>
         </div>
-        <BaiduMap 
-          mapContainer={<div style={{ height: '100%', width: '100%' }} />} 
-          zoom={7} 
-          center={this.state.center}
-          enableScrollWheelZoom
-        >
-          {
-            fetchIntransitSocking ? 
-            <CenterLoading className='center-loading' text='在途数据加载中...'/> :
-            list ? 
-            list.map((item, index) => (
-              <Overlay key={item.vesselExportEname} 
-                  constructorParams={{
-                    point: {
-                      lng: item.longitude,
-                      lat: item.latitude
-                    },
-                    text: `运输工具: ${item.vesselExportEname}/${item.exportVoyage}`,
-                    extra:  `货物: ${item.cargoShortName},${item.feeWeight}吨`,
-                    index
-                  }}
-                  {...createOverlayMethods}
-                />
-            )) : null
-          }
-        </BaiduMap>
+        <div style={{ height: '100%', width: '100%', position: 'relative', zIndex: 1 }}>
+          <BaiduMap 
+            mapContainer={<div style={{ height: '100%', width: '100%' }} />} 
+            zoom={this.state.zoom} 
+            center={this.state.center}
+            enableScrollWheelZoom
+          >
+            {
+              fetchIntransitSocking ? 
+              <CenterLoading className='center-loading' text='在途数据加载中...'/> :
+              list ? 
+              list.map((item, index) => (
+                <Overlay key={`lng${item.longitude}_lat${item.latitude}`} 
+                    constructorParams={{
+                      point: {
+                        lng: item.longitude,
+                        lat: item.latitude
+                      },
+                      text: `运输工具: ${item.vesselExportEname || item.vesselImportEname || '未知'}/${item.exportVoyage || item.importVoyage || '未知'}`,
+                      extra:  `货物: ${item.cargoShortName},${item.weight || '0'}吨`,
+                      index
+                    }}
+                    {...createOverlayMethods}
+                  />
+              )) : null
+            }
+          </BaiduMap>
+        </div>
         <Modal
           visible={this.state.visible}
           popup
@@ -185,17 +254,26 @@ class WharfSock extends Component {
           onClose={() => this.handleShowModal()}
           afterClose={() => document.documentElement.style.overflow=''}
         >
-          <h2 className='popup-list-title'>{this.state.detail['vesselExportEname']}/{this.state.detail['exportVoyage']}</h2>
+          <h2 className='popup-list-title'>{this.state.detail['vesselExportEname'] || this.state.detail['vesselImportEname'] || '未知'}/{this.state.detail['exportVoyage'] || this.state.detail['importVoyage'] || '未知'}</h2>
           <Fields
             columns={columns}
             data={this.state.detail}
             labelWidth={116}
           />
         </Modal>
+        <Drawer
+          style={{zIndex:this.state.zIndex}}
+          open={this.state.list}
+          onOpenChange={this.handleListOpenChange}
+          sidebar={this.renderTransportList()}
+          position='right'
+        >
+          <span></span>
+        </Drawer>
       </Screen>
     )
   }
 }
 
-export default WharfSock;
+export default TransportSock;
 
